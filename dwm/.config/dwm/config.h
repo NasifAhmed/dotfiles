6bafb4c008c86ad9bb07f1719a769bc728b89b6e
Nasif Ahmed <nasif2ahmed@gmail.com>
2020-07-11 11:23:35 +0600

    Added dmenu from suckless website and patched

--- dwm/.config/dwm/config.h
+++ dwm/.config/dwm/config.h
@@ -71,7 +71,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray5, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-i", "-h", "50", "-l", "8", "-fn", dmenufont, "-nb", "#000000", "-nf", "#a89984", "-sb", "#282828", "-sf", "#fbf1c7", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *browser[]  = { "brave", NULL };
 static const char *lock[] = { "slock", NULL};
