c0f1e07b3c0157fc0d0cd1a7d99ed152040eed7e
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-03-04 09:55:20 +0600

    Add colorscheme and cursorline change

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,17 @@
 -- Relative line numbers
 vim.opt.nu = true
-vim.opt.relativenumber = true
+--vim.opt.relativenumber = true
 
 -- 4 space indent
 vim.opt.tabstop = 4  
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = false
+
+-- Enable cursorline and cusromcolumn
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
 
 -- Increamental search but doesn't stay highlighted
 vim.opt.hlsearch = false
@@ -16,6 +19,7 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
+-- Stop scrolling to the bottom. Always keep 8 line padding.
 vim.opt.scrolloff = 8
 
 -- Decrease update time
@@ -31,3 +35,6 @@ vim.opt.breakindent = true
 -- Save undo history
 vim.opt.undofile = true
 
+
+-- Set colorscheme from default ones
+vim.cmd.colorscheme('habamax')
